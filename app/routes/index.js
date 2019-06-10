4fc420544887fd6d54ea041219a3a036944a8f6e
James L. Makela <james.makela@noaa.gov>
2019-06-09 21:22:57 -0700

    The inital findAll() is not needed with light-table, just a recordArray obj.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('user');
+    // We do not want to make a request to the backend yet, so we use
+    // peekAll() which simply gets local records in the store.
+    return this.store.peekAll('user');
   }
 });
